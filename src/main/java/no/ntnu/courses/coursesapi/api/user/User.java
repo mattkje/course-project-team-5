9764abf88047d6c1a41f818145886cf7670415ff
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-03-05 19:25:09 +0100

    Design document update

--- src/main/java/no/ntnu/courses/coursesapi/api/user/User.java
+++ src/main/java/no/ntnu/courses/coursesapi/api/user/User.java
@@ -1,7 +1,14 @@
 package no.ntnu.courses.coursesapi.api.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Date;
 
 
