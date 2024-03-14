ef231130a4e87e7d953c1b59f03905ca6d764ef8
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-03-14 11:35:29 +0100

    Design changes 2

--- src/main/java/no/ntnu/courses/coursesapi/CoursesApiApplication.java
+++ src/main/java/no/ntnu/courses/coursesapi/CoursesApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoursesApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CoursesApiApplication.class, args);
     }
