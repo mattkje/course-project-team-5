bf64fda590939f2a035115dcf52e87971684621f
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-03-18 20:29:50 +0100

    Added similar courses in details page

--- src/main/resources/static/scripts/course.js
+++ src/main/resources/static/scripts/course.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     loadComponent('footer');
-    loadComponent('menubar')
+    loadComponent('menubar');
     populateCoursePage();
 };
 
@@ -13,6 +13,7 @@ function loadComponent(component) {
 }
 
 // Function to populate the course page with the course data
+
 function populateCoursePage() {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
@@ -21,7 +22,7 @@ function populateCoursePage() {
     fetch(`/api/courses/${id}`)
         .then(response => response.json())
         .then(data => {
-
+            populateCourses('.featured');
             document.getElementById('courseCategoryLink').innerText = data.course.category;
             document.getElementById('courseTitleLink').innerText = data.course.title;
 
@@ -159,3 +160,118 @@ function formatDate(closestCourseSession) {
 
     return `${formattedStartDate} -> ${formattedEndDate}`;
 }
+
+function populateCourses(selector) {
+    fetch('/api/courses')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(courseProvider => {
+                if (document.querySelector(selector).childElementCount >= 5) {
+                    return false;
+                }
+                if (courseProvider.course.category === document.getElementById('courseCategoryLink').innerText) {
+                    const contentBox = document.createElement('a');
+                    contentBox.href = `/courses?id=${courseProvider.course.courseId}`;
+                    contentBox.className = 'content-box';
+
+                    const image = document.createElement('img');
+                    image.src = courseProvider.course.image;
+                    image.alt = 'Course Logo';
+                    image.className = 'content-box-image';
+                    contentBox.appendChild(image);
+
+                    const descriptionBox = document.createElement('div');
+                    descriptionBox.className = 'content-box-description';
+                    contentBox.appendChild(descriptionBox);
+
+                    const title = document.createElement('h2');
+                    title.className = 'content-box-title';
+                    title.textContent = courseProvider.course.title;
+                    descriptionBox.appendChild(title);
+
+                    const hr = document.createElement('hr');
+                    descriptionBox.appendChild(hr);
+
+                    const contentDescription = document.createElement('div');
+                    contentDescription.className = 'content-description';
+                    descriptionBox.appendChild(contentDescription);
+
+                    const attributes = document.createElement('div');
+                    attributes.className = 'content-box-attributes';
+                    contentDescription.appendChild(attributes);
+
+                    // Create and append the category attribute
+                    const categoryAttribute = document.createElement('div');
+                    categoryAttribute.className = 'content-box-attribute';
+                    attributes.appendChild(categoryAttribute);
+
+                    const categoryIcon = document.createElement('img');
+                    categoryIcon.className = 'content-box-icon';
+                    categoryIcon.src = 'media/category.svg'; // Replace with actual path
+                    categoryAttribute.appendChild(categoryIcon);
+
+                    const category = document.createElement('p');
+                    category.className = 'content-box-text';
+                    category.textContent = courseProvider.course.category;
+                    categoryAttribute.appendChild(category);
+
+                    // Create and append the providers attribute
+                    const providersAttribute = document.createElement('div');
+                    providersAttribute.className = 'content-box-attribute';
+                    attributes.appendChild(providersAttribute);
+
+                    const providersIcon = document.createElement('img');
+                    providersIcon.className = 'content-box-icon';
+                    providersIcon.src = 'media/providers.svg';
+                    providersAttribute.appendChild(providersIcon);
+
+                    const providersElement = document.createElement('p');
+                    providersElement.className = 'content-box-text';
+
+
+                    const courseProviders = courseProvider.providers.filter(provider => provider.courseId === courseProvider.course.courseId);
+
+                    if (Array.isArray(courseProviders) && courseProviders.length) {
+                        providersElement.innerHTML = `${courseProviders.length}&nbsp;Providers`;
+                    } else {
+                        providersElement.innerHTML = 'No&nbsp;Providers';
+                    }
+
+                    providersAttribute.appendChild(providersElement);
+
+                    const price = document.createElement('p');
+                    price.className = 'content-button';
+                    const lowestPriceProvider = courseProviders.reduce((prev, curr) => {
+                        return (prev.price < curr.price) ? prev : curr;
+                    });
+
+                    const currency = lowestPriceProvider.currency;
+
+                    if (currency === 'USD') {
+                        price.textContent = "$" + lowestPriceProvider.price;
+                    } else if (currency === 'EUR') {
+                        price.textContent = "€" + lowestPriceProvider.price;
+                    } else if (currency === 'GBP') {
+                        price.textContent = "£" + lowestPriceProvider.price;
+                    } else if (currency === 'SUB') {
+                        price.textContent = "$" + lowestPriceProvider.price + "/month";
+
+                    } else {
+                        price.textContent = currency + " " + lowestPriceProvider.price;
+                    }
+
+                    const priceBox = document.createElement('div');
+                    priceBox.className = 'price-box';
+                    priceBox.appendChild(price);
+
+                    contentDescription.appendChild(priceBox);
+
+                    document.querySelector(selector).appendChild(contentBox.cloneNode(true));
+
+                }
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
