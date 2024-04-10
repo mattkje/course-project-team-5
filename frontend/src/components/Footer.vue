13c8a7293416ea44b3b35402a8b8b33f27c0d9bd
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-04-10 12:41:36 +0200

    Changed to use Vue.js as frontend framework

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,86 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    <div class="footer-content">
+      <div class="footer-section">
+        <h5>Contact Us</h5>
+        <p>Email: info@learniverse.com</p>
+        <p>Phone: +47 70 70 70 70</p>
+      </div>
+      <div class="footer-section">
+        <div class="footer-section">
+          <h5>Created by Team 5 at</h5>
+          <img width="200px" src="/ntnu2.svg" alt="NTNU Logo">
+        </div>
+        <div class="footer-logo">
+          <img class="logo-footer" src="/logo.svg" alt="Connect">
+          <h4 class="logo-top">Learniverse&nbsp;</h4>
+        </div>
+      </div>
+      <div class="footer-section">
+        <h5>Useful Links</h5>
+        <a href="/categories" target="_blank">Categories</a>
+        <a href="/explore" target="_blank">Explore</a>
+        <a href="/about" target="_blank">About us</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+footer {
+  font-family: 'Inter', sans-serif;
+  background: linear-gradient(180deg, rgba(21, 16, 82, 0.14) 100%, rgba(158, 150, 255, 0.14) 10%);
+  color: #EAEAEA;
+  text-align: center;
+  padding: 20px;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.footer-section {
+  width: 220px;
+  margin: 10px 0;
+}
+
+footer a {
+  font-family: 'Inter', sans-serif;
+  color: #EAEAEA;
+  text-decoration: none;
+  display: block;
+  margin: 5px 0;
+}
+
+footer .logo-top {
+  color: #EAEAEA;
+  text-decoration: none;
+  display: block;
+  margin: 5px 0;
+}
+
+footer a:hover {
+  text-decoration: underline;
+}
+
+.footer-logo {
+  border: none;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+}
+
+.logo-footer {
+  width: 40px;
+  height: 40px;
+}
+
+</style>
+
---
+++ frontend/src/components/Menubar.vue
@@ -0,0 +1,199 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="menubar">
+    <div class="header">
+      <div class="content">
+        <div class="left-content">
+          <a href="/" class="header-button">
+            <img class="logo" src="/logo.svg" alt="Connect">
+            <h4 class="logo-top">Learniverse&nbsp;</h4>
+            <h4 class="logo-bottom">Connect</h4>
+          </a>
+          <div class="menu-links">
+            <a href="/explore" class="header-button">Explore</a>
+            <a href="/community" class="header-button">Community</a>
+            <a href="/pro" class="header-button">Learniverse&nbsp;Pro</a>
+          </div>
+        </div>
+
+        <div class="right-content">
+          <a href="/cart" class="fancy-button">
+            <img class="cart" src="/cart.svg" alt="Cart">
+          </a>
+          <select class="fancy-button" id="currencySelect" @change="updateCurrency">
+            <option v-for="currency in currencies" :value="currency.code">
+              {{ currency.code }} - {{ currency.symbol }}
+            </option>
+          </select>
+          <a href="/register" class="standard-button">Get Started</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currencies: [],
+      defaultCurrency: null,
+    };
+  },
+  methods: {
+    fetchCurrencies() {
+      fetch('http://localhost:8080/api/currency')
+          .then(response => response.json())
+          .then(currencies => {
+            this.currencies = currencies;
+            this.defaultCurrency = this.setDefaultCurrency();
+          })
+          .catch(error => console.error('Error:', error));
+    },
+    setDefaultCurrency() {
+      const cookies = document.cookie.split('; ');
+      const defaultCurrencyCookie = cookies.find(row => row.startsWith('defaultCurrency='));
+      if (defaultCurrencyCookie) {
+        return defaultCurrencyCookie.split('=')[1];
+      }
+    },
+    updateCurrency(event) {
+      document.cookie = `defaultCurrency=${event.target.value}; path=/; max-age=31536000`;
+      location.reload();
+    },
+  },
+  mounted() {
+    this.fetchCurrencies();
+  },
+};
+</script>
+
+<style scoped>
+
+.menubar {
+  background-color: rgba(6, 6, 16, 0.9);
+  backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.08);
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 100;
+}
+
+.content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 70px;
+}
+
+.left-content {
+  max-height: 70px;
+  padding: 0 2vw;
+  display: flex;
+  align-items: center;
+}
+
+.right-content {
+  max-height: 70px;
+  margin: 3vw;
+  display: flex;
+  max-width: min-content;
+  align-items: center;
+}
+
+.header-button {
+  height: 70px;
+  color: white;
+  font-size: 15px;
+  border: none;
+  padding: 10px 1%;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  margin-right: 3vw;
+}
+
+.right-content .header-button {
+  margin-right: 0;
+  margin-left: 3vw;
+}
+
+.header-button:hover {
+  opacity: 60%;
+}
+
+.menu-links {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 50px;
+}
+
+.logo {
+  width: 60px;
+  height: 60px;
+  padding-left: 20px;
+  transform: translateY(-3px);
+}
+
+.logo-top {
+  font-weight: 900;
+  padding-left: 5px;
+}
+
+.logo-bottom {
+  color: #584BEB;
+}
+
+.cart {
+  width: 25px;
+  height: 25px;
+  margin: 0;
+  padding: 0;
+}
+
+.fancy-button {
+  color: var(--light-1);
+  font-family: 'Inter', sans-serif;
+  font-weight: bold;
+  padding: 10px;
+  border-radius: 10px;
+  text-decoration: none;
+  display: flex;
+  margin: 0 10px;
+  max-height: 50px;
+  background: #0C0C0C;
+  border: 0.5px solid #252525;
+
+}
+
+.fancy-button:hover {
+  background-color: #262626;
+  box-shadow: 0 -1px 0 rgba(0, 0, 0, .04), 0 2px 4px rgba(0, 0, 0, .25);
+}
+
+.standard-button {
+  background-color: #584BEB;
+  color: #EAEAEA;
+  font-weight: bold;
+  padding: 10px;
+  border-radius: 10px;
+  text-decoration: none;
+  display: flex;
+  width: max-content;
+
+  &:hover {
+    background-color: #6E67FC;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, .04), 0 2px 4px rgba(0, 0, 0, .25);
+  }
+}
+</style>
