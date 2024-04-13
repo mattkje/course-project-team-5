70178d743cb96f13caf7b82cd4531c29f023f3b8
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-04-13 12:20:58 +0200

    Google Maps API fix

--- frontend/src/views/Course.vue
+++ frontend/src/views/Course.vue
@@ -54,7 +54,6 @@
     <div id="map-container" class="map-container" style="display: none">
       <h3>Course Location</h3>
       <div id="map" ></div>
-
     </div>
 
 
@@ -83,7 +82,7 @@ const API_URL = appContext.config.globalProperties.$apiAddress;
 
 // Google Maps API (Looks like nonsense, but is not. So Do Not Delete!)
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});
+({key: "AIzaSyAhRVHfIjsffeR6UTu-ypmHJvnlZqg5a2s", v: "weekly"});
 
 // Function to populate the course page with the course data
 function populateCoursePage() {
