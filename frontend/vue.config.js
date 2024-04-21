fed3e4647a0937d696b0b09507462258d46a518d
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-04-21 14:36:56 +0200

    Added some dummy cert files

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs');
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    server: {
+      type: 'https',
+      options: {
+        key: fs.readFileSync('./key.pem'),
+        cert: fs.readFileSync('./cert.pem'),
+      },
+    },
+    port: 443,
+  },
 })
