a6fb9413ab70afbd948e913a296e171a22250f24
Matti Kjellstadli <40216910+mattkje@users.noreply.github.com>
2024-05-19 22:25:22 +0200

    Corrected broken link

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,5 +10,5 @@ const app = createApp(App)
 // Content will not load on external machines if this address is localhost.
 // Also check the address in the backend server before starting.
 //app.config.globalProperties.$apiAddress = 'https://connect.learniverse.store:8443/api';
-app.config.globalProperties.$apiAddress = 'http://localhost:8080/api';
+app.config.globalProperties.$apiAddress = 'https://localhost:8443/api';
 app.use(router).mount('#app')
