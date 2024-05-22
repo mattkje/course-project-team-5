80e35a7ed304d3eaafc2184e4611cef60b3d27e5
HSK <91470652+AbsolutelyHaakon@users.noreply.github.com>
2024-05-22 14:40:10 +0200

    Checkstyle complete

--- backend/src/main/java/no/ntnu/api/config/AccessUserDetails.java
+++ backend/src/main/java/no/ntnu/api/config/AccessUserDetails.java
@@ -1,15 +1,18 @@
 package no.ntnu.api.config;
 
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import no.ntnu.api.role.Role;
 import no.ntnu.api.user.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.*;
-
 /**
- * Contains authentication information, needed by UserDetailsService
+ * Contains authentication information, needed by UserDetailsService.
  */
 public class AccessUserDetails implements UserDetails {
   private final String username;
@@ -29,6 +32,11 @@ public class AccessUserDetails implements UserDetails {
     this.convertRoles(user.getRoles());
   }
 
+  /**
+   * Convert roles to authorities.
+   *
+   * @param roles The roles to convert
+   */
   private void convertRoles(Set<Role> roles) {
     authorities.clear();
     for (Role role : roles) {
@@ -36,36 +44,71 @@ public class AccessUserDetails implements UserDetails {
     }
   }
 
+  /**
+   * Get the username.
+   *
+   * @return The username
+   */
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
     return authorities;
   }
 
+  /**
+   * Get the password.
+   *
+   * @return The password
+   */
   @Override
   public String getPassword() {
     return password;
   }
 
+  /**
+   * Get the username.
+   *
+   * @return The username
+   */
   @Override
   public String getUsername() {
     return username;
   }
 
+  /**
+   * Check if the account is active.
+   *
+   * @return True if the account is active
+   */
   @Override
   public boolean isAccountNonExpired() {
     return isActive;
   }
 
+  /**
+   * Check if the account is locked.
+   *
+   * @return True if the account is not locked
+   */
   @Override
   public boolean isAccountNonLocked() {
     return isActive;
   }
 
+  /**
+   * Check if the credentials are expired.
+   *
+   * @return True if the credentials are not expired
+   */
   @Override
   public boolean isCredentialsNonExpired() {
     return isActive;
   }
 
+  /**
+   * Check if the account is enabled.
+   *
+   * @return True if the account is enabled
+   */
   @Override
   public boolean isEnabled() {
     return true;
